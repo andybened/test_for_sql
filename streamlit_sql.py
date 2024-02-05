fa823b7fbda61c4a40b036fd1fc06f77019f68f7
andybened <141198932+andybened@users.noreply.github.com>
2024-02-05 09:56:52 +0800

    Add files via upload

---
+++ streamlit_sql.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 17:54:33 2024
+
+@author: User
+"""
+
+import pandas as pd
+import pyodbc
+import streamlit as st
+
+# database variables
+# sql_server  = 'DESKTOP-86RO9TM'
+# database    = 'AA_database'
+# username    = 'sa'
+# password    = '96328521'
+# driver      = '{ODBC Driver 18 for SQL Server}'
+conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"])
+cursor = conn.cursor()
+tech_text = pd.read_sql(sql="EXEC shopline_order_source" , con=conn)
+conn.commit()
+
+st.write(tech_text)
